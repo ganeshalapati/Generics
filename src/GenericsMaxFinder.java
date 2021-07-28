61a40bfaf05e6af2becb50c221088e324fe1e4e6
ganeshalapati <alapatiganesh31@gmail.com>
2021-07-29 04:23:01 +0530

    Added method to Generics class

--- src/GenericsMaxFinder.java
+++ src/GenericsMaxFinder.java
@@ -1,50 +1,37 @@
 
-public class GenericsMaxFinder {
-	//main  method
-		public static void main(String[] args) 
-		{
-			Integer a=2,b=7,c=5;
-			Float p=4.f,q=2.3f,r=9.8f;
-			String x="ab",y="abc",z="abcd";
-			findMaxInt(a,b,c); 
-			findMaxFloat(p,q,r); 
-			findMaxString(x,y,z); 
-			
-			
-		}
-		//max of three Int
-		public static void findMaxInt(Integer a,Integer b,Integer c)
-		{
-			Integer max=a;
-			if(b.compareTo(max)>0)
-				max=b;
-			else if(c.compareTo(max)>0)
-				max=c;
-			System.out.printf("maximum of numbers: [%s ,%s, %s]is %s ",a,b,c,max);			
-		}
-		//max of three Float
-		public static void findMaxFloat(Float p,Float q,Float r)
-		{
-			Float max=p;
-			 if (q.compareTo(max)>0) {
-		            max = q;
-		        }
-			 else if (r.compareTo(max)>0) {
-		            max = r;
-		        }
-		        System.out.printf("Max value of %s %s %s is : %s\n", p,q,r, max);			
-		}
-		//max of three String
-		public static void findMaxString(String x,String y,String z)
-		{
-			String max=x;
-			 if (y.compareTo(max)>0) {
-		            max = y;
-		        }
-			 else if (z.compareTo(max)>0) {
-		            max = z;
-		        }
-		        System.out.printf("Max value of %s %s %s is : %s\n", x,y,z ,max);			
-		}
+public class  GenericsMaxFinder <T extends Comparable<T>> {
+
+	T num1;
+	T num2;
+	T num3;
+	//constructor
+    public GenericsMaxFinder(T num1, T num2, T num3) {
+		super();
+		this.num1 = num1;
+		this.num2 = num2;
+		this.num3 = num3;
+	}
+
+
+    public T GenericMax() {
+    	return GenericsMaxFinder.GenericMax(num1, num2, num3);
+    }
+
+    // Generic method to find max of any numbers
+    public static <T extends Comparable<T>> T GenericMax(T num1, T num2, T num3) {
+        T max = num1;
+        if (num2.compareTo(max) > 0)
+            max = num2;
+        if (num3.compareTo(max) > 0)
+            max = num3;
+        return max;
+    }
+
+	// Main method
+    public static void main(String[] args) {
+    	  System.out.println("Maximum Integer by creating Generic class :" + new GenericsMaxFinder(3, 6, 2).GenericMax());
+    	  System.out.println("Maximum Float by creating Generic class :" + new GenericsMaxFinder(3.5f, 6.56f, 3.45f).GenericMax());
+    	  System.out.println("Maximum Float by creating Generic class :" + new GenericsMaxFinder("abc","abcd","abcde").GenericMax());
+    }
 
 }
